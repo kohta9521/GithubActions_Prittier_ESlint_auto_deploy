f1f246976aaf5061c727cacc8477e5b5829e896d
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-19 11:49:15 +0900

    feat(app.js index.js): useContextの実装
    Comicinfoのやりとり

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { useState, useEffect, useContext } from 'react'
 import './App.css'
+import ComicContext from './index'
 
 function App() {
+  // useState
+  const [count, setCount] = useState(0)
+
+  // context
+  const comicInfo = useContext(ComicContext)
+
+  // handleClick function
+  const handleClick = () => {
+    setCount((prev) => prev + 1)
+  }
+
+  // useEffect
+  useEffect(() => {
+    console.log('Hello useeffect!!!')
+  }, [count])
+
   return (
     <div className="App">
-      <h1>React Hooks Qiita Lesson</h1>
-      <p>sample code</p>
+      <h1>useState</h1>
+      <button onClick={handleClick}>count is {count}</button>
+      <p>{comicInfo.title}</p>
+      <p>{comicInfo.author}</p>
     </div>
   )
 }
